4b6ace01731e6bc477d7c5b66d0b1cecfa2e3845
sophiecrilly <crilly.sophie16@gmail.com>
2020-03-17 11:47:14 +1300

    working hamburger menu, working on button

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// alert('javascript has loaded');
+
+// $
+// jQuery
+
+// $(selector).effect()
+            // .action
+            // .event
+
+
+
+
+
+$('#hamburgerIcon').click(function(){
+    $('#hamburgerNav').toggle();
+    // $('#hamburgerNav').removeClass('hidden');
+
+});
